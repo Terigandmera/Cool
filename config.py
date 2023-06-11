44ce78ad42f6108ce31e45e4bd6d9723fbfe199c
Vivek <123234657+VivekCoderOp@users.noreply.github.com>
2023-06-12 05:07:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Dora")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "yukki")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5656382791").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/VivekNoobOp/NewThumb")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vivekaakash/NewThumb")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
